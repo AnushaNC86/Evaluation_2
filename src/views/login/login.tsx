51f9af4724fb3e8247e18806bfbea46f84652cbd
AnushaNC86 <anusha.nc@robosoftin.com>
2022-10-17 09:59:53 +0530

    Saved

---
+++ src/views/login/login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="loginContainer">
+      <div className="logo">logo</div>
+      <div className="formContianer">form</div>
+    </div>
+  );
+};
+
+export default Login;
