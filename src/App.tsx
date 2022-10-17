6b1d31a6be913b2a935a10c0a969549d70276195
AnushaNC86 <anusha.nc@robosoftin.com>
2022-10-17 18:58:10 +0530

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import Login from "./views/login/loginPage";
 import "./style.css";
-import { Routes, Route } from "react-router-dom";
-import Register from "./views/Register/register";
+import Router from "./components/Router/router";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Register />} />
-      </Routes>
+      <Router />
     </div>
   );
 }
