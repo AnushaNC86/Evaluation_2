0973e2f3c578399a2b03fcf99cb0bea7f4bc67ec
AnushaNC86 <anusha.nc@robosoftin.com>
2022-10-18 12:29:32 +0530

    Saved

--- src/components/sidebar/sideBar.tsx
+++ src/components/sidebar/sideBar.tsx
@@ -1,5 +1,14 @@
+import "./sidebar.css";
+
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <div className="sideBarContainer">
+      <div className="logoBurger">
+        <img src={require("../../assets/icons/burger_Menu.png")} alt="" />
+      </div>
+      <div className="homeIcn"></div>
+    </div>
+  );
 };
 
 export default SideBar;
--- src/views/home/homepage.tsx
+++ src/views/home/homepage.tsx
@@ -1,10 +1,13 @@
+import SideBar from "../../components/sidebar/sideBar";
 import "./homePage.css";
 
 const HomePage = () => {
   return (
     <div className="home">
       <div className="homeContainer">
-        <div className="sideBar">Sidebar</div>
+        <div className="sideBar">
+          <SideBar />
+        </div>
         <div className="topBar">Top</div>
         <div className="Body">Body</div>
       </div>
